0b242bdb776fdd2136b29c8bd297728baad4cdeb
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-11-23 08:57:55 +0700

    rubocop fix

--- reader.rb
+++ reader.rb
@@ -2,7 +2,7 @@ class Reader
   def read_action(game)
     loop do
       game.action_item = gets.chomp.to_i
-      return unless (game.action_item < 0) || (game.action_item > 9)
+      return unless game.action_item.negative? || (game.action_item > 9)
 
       puts('error, no such action')
     end
