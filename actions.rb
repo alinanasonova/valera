7116c3dd525d052de8769fe21e91791ce189bd7e
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-11-23 08:56:00 +0700

    added conds

--- actions.rb
+++ actions.rb
@@ -1,14 +1,46 @@
 require_relative 'valera'
 
 class Actions
-  def self.execute(status, action)
-    case action
-      when 1..7 then action.each { |field, key| status[field] = status[field].send key['operator'],key['value'].to_i }
-      when 8 then Saver.save_load_menu(status, 7)
-      when 9 then Saver.save_load_menu(status, 8)
-      when 0 then exit
+  @act = Actions.new
+  def sender(field, key)
+    field.send key['operator'], key['value'].to_i
   end
 
+  # def check_cond_status(status, action)
+  #  status.each_value{ |value| if !value then return false end }
+  #  return true
+  # end
+  def action_cond(status, action)
+    action.each { |field, key| return false unless sender(status[field], key) }
+    # check_cond_status(status, action)
+    true
+  end
+
+  def conds_then_field(status, field, key)
+    if key.key?('conds') && action_cond(status, key['conds'])
+      sender(field, key['then'])
+    else
+      sender(field, key)
+    end
+  end
+
+  def change_status(status, action)
+    action.each { |key, value| status[key] = conds_then_field(status, status[key], value) }
+    status
+  end
+
+  def selected_action(status, action)
+    unless action['conds'].nil?
+      return unless action_cond(status, action['conds'])
+
+      status = change_status(status, action['effects'])
+    end
+    status
+  end
+
+  def self.execute(status, action)
+    @act.selected_action(status, action)
+  end
   # def self.go_work(status)
   #   status['happienss'] -= 5
   #   status['mana'] -= 30
