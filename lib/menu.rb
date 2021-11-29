b519ee72d27680ca8926d2ded90da3ec53d55e8c
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-11-29 20:58:47 +0700

    deleted old menu

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,15 +9,6 @@ class Menu
   end
   def print_actions
     @arr_action.map.with_index { |action, i| print "(#{i+1}) - #{action}\n" }
-    # puts '(1) - Go to work'
-    # puts '(2) - See nature'
-    # puts '(3) - Drink wine and see serial'
-    # puts '(4) - Go to bar'
-    # puts '(5) - Drink with marginals'
-    # puts '(6) - Sing in the subway'
-    # puts '(7) - Go to sleep'
-    puts '(8) - Save game'
-    puts '(9) - Load game'
     puts '(0) - Exit'
   end
 end
