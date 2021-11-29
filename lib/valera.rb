55ce7e2e39aa045fd71f3821b94b34895ddf2fa4
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-11-30 03:05:13 +0700

    'check_status' -> 'dead?'

--- lib/valera.rb
+++ lib/valera.rb
@@ -1,7 +1,7 @@
 class Valera
   attr_accessor :status
 
-  def initialize(health = 100, mana = 50, happienss = 0, money = 50, fatigue = 50)
+  def initialize(health = 100, mana = 50, happienss = 5, money = 100, fatigue = 0)
     @status = {
       'health' => health,
       'mana' => mana,
@@ -11,19 +11,19 @@ class Valera
     }
   end
 
-  def check_status(status)
-    fix_status!(status)
-    if check_gameover
-      @status = status
-      return true
-    end
+  def dead?(status)
+    return true if check_gameover(status)
+
     false
   end
-  def check_gameover
-    return false if status['health'] < 0
-    return false if status['fatigue'] > 100
-    return true
+
+  def check_gameover(status)
+    return true if status['health'].negative?
+    return true if status['fatigue'] > 100
+
+    false
   end
+
   def fix_status!(status)
     status['health'] = 100 if status['health'] > 100
     status['mana'] = 0 if (status['mana']).negative?
