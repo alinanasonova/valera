69fc88bcf777298924e078acda89cb3023c4d8a5
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-11-30 05:46:26 +0700

    add spec

---
+++ spec/reader_spec.rb
@@ -0,0 +1,14 @@
+require './lib/reader'
+
+RSpec.describe Reader do
+  
+  describe '#read_action' do
+    it 'Input choice by user' do
+      game = Game.new
+      expect do
+        allow(game).to receive(:gets).and_return('2')
+        expect(Reader.new.read_action(game)).to eq nil
+      end
+    end
+  end
+end
